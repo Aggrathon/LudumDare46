05deae2801eecf6229811e2184aab0fe141fab48
Aggrathon <antonbjo@gmail.com>
2020-04-20 02:32:14 +0300

    theme + main menu + tutorial

---
+++ Assets/Scripts/MusicButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicButton : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    public void OnMusicClick() {
+        if (audioSource.isPlaying)
+            audioSource.Pause();
+        else
+            audioSource.Play();
+    }
+}
